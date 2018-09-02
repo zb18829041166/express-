9993caf85f25673526893c010ef017f364d99fa5
zb <2479107942@qq.com>
2018-09-02 21:43:12 +0800

    添加了静态服务器

--- static/js/b.js
+++ static/js/b.js
@@ -1,7 +1,1 @@
-
-var xhr = new XMLHttpRequest()
-xhr.open('GET', '/getWeather?city=hangzhou', true)
-xhr.send()
-xhr.onload = function(){
-  console.log(JSON.parse(xhr.responseText))
-}
+alert('hello')
